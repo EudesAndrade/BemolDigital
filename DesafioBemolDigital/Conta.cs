22378ba2501a5fbbfe217321865d805c5f7f3da6
EudesAndrade <eudesgomesandrade@gmail.com>
2020-07-24 19:27:52 -0400

    Check in final

--- DesafioBemolDigital/Conta.cs
+++ DesafioBemolDigital/Conta.cs
@@ -32,7 +32,7 @@ namespace DesafioBemolDigital
 
                 this.mskCEP.Focus();
 
-                return;//teste
+                return;
             }
 
             if (mskCEP.TextLength == 9)
